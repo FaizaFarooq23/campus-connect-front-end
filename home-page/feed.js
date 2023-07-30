583ae2403b1d9b8f8a15df29638995996dd4c3cc
FaizaFarooq23 <faizafarooq2325@gmail.com>
2023-07-30 13:29:47 +0500

    home-page created

---
+++ home-page/feed.js
@@ -0,0 +1,44 @@
+for (let i = 0; i < users.length; i++) {
+    const feed_div = document.createElement("div");
+    feed_div.classList.add("feed-div");
+    feed_div.innerHTML = `
+    <div class="feed">
+        <div class="feed-header">
+            <div class="feed-header-left">
+                <img src=${users[i].image} alt="user-profile-photo">
+                <span class="">${users[i].name}</span>
+            </div>
+            <div class="feed-time">
+            <div class="dot"></div>
+            <span class="time">1 day ago</span>
+        </div>
+            <div class="feed-header-right">
+                <img src="icons/dots.svg" class="dot-img" alt="dots-svg"></img>
+            </div>
+        </div>
+        <div class="feed-image">
+            <img src=${users[i].post} alt="user-uploaded-photo" class="user-feed-image">
+        </div>
+        <div class="feed-footer">
+            <div class="feed-footer-left">
+                <i class="far fa-heart"></i>
+                <i class="far fa-comment"></i>
+                <i class="far fa-paper-plane"></i>
+            </div>
+            <div class="feed-footer-right">
+                <i class="far fa-bookmark"></i>
+            </div>
+        </div>
+        <div class="feed-likes">
+            <span class="likes">Liked by <span class="like-name">${users[i].name}</span> and <span class="others">others</span></span>
+        </div>
+        <div class="feed-caption">
+            <span class="name">${users[i].name}</span>
+            <span class="caption">${users[i].caption}</span>
+        </div>
+    <div class="line"></div>
+    </div>
+    `;
+    feed.appendChild(feed_div);
+
+}
---
+++ home-page/stories.js
@@ -0,0 +1,56 @@
+const stories_div = document.getElementById("story-div");
+const feed = document.getElementById("feed");
+const users = [
+    {
+        name: "Faiza Farooq",
+        image: "https://picsum.photos/200",
+        post: "https://picsum.photos/290",
+        caption: "Sunset in the mountains #mountains #sunset #photography",
+    },
+    {
+        name: "Hira Batool",
+        image: "https://picsum.photos/300",
+        post: "https://picsum.photos/390",
+        caption:"flowers #flowers #photography",
+    },
+    {
+        name: "Moazma Ehsan",
+        image: "https://picsum.photos/400",
+        post: "https://picsum.photos/308",
+        caption:"Cute cat ",
+    },
+    {
+        name: "Hafsa Shamraiz",
+        image: "https://picsum.photos/500",
+        post: "https://picsum.photos/409",
+        caption:"Nature "
+    },
+    {
+        name: "Jo b",
+        image: "https://picsum.photos/600",
+        post: "https://picsum.photos/900",
+        caption:"No caption "
+    },
+    {
+        name: "Mai ni btaunga",
+        image: "https://picsum.photos/700",
+        post: "https://picsum.photos/800",
+        caption:"What to write? "
+    }
+]
+
+for (let i = 0; i < users.length; i++) {
+    const story = document.createElement("div");
+    story.classList.add("story-name");
+    story.innerHTML = `
+    <div class="circle">
+            <div class="story">
+            <img src=${users[i].image}  alt="user-profile-photo">
+        </div> `;
+    const name = document.createElement("span");
+    name.classList.add("name");
+    name.innerText = users[i].name.split(" ")[0] ;
+    story.appendChild(name);
+    stories_div.appendChild(story);
+}
+
